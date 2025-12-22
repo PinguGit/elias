5c0b4ffeaef627c2a7196b39f6f5149690278313
Pingu <opsb65525@gmail.com>
2025-12-22 11:55:25 +0100

    config angepasst

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Export static files for GitHub Pages
+  output: 'export',
+  // Project is hosted at https://pingugit.github.io/elias
+  basePath: '/elias',
+  assetPrefix: '/elias/',
+  trailingSlash: true,
 };
 
 export default nextConfig;
